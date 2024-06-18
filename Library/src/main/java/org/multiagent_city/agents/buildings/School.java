b837a8331fd841028c9a56d82870b1a2f44070ac
Anna DI PLACIDO <anna.di-placido@etu.unice.fr>
2024-06-19 00:26:15 +0200

    type dans differents buildings

--- Library/src/main/java/org/multiagent_city/agents/buildings/School.java
+++ Library/src/main/java/org/multiagent_city/agents/buildings/School.java
@@ -1,9 +1,10 @@
 package org.multiagent_city.agents.buildings;
 
 import org.multiagent_city.agents.Building;
+import org.multiagent_city.infrastructure.InfrastructureType;
 
 public class School extends Building {
-    public School(int capacity) {
-        super(capacity);
+    public School(int capacity, InfrastructureType type) {
+        super(capacity,type);
     }
 }
